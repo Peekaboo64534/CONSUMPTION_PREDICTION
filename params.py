280629c1581da50ce3fa1f1efe4ab0ef3da551aa
Thais Gaspar <thagaspar@hotmail.com>
2024-06-04 18:03:10 +0100

    projectday1

---
+++ params.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+
+
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
+GCP_REGION = os.environ.get("GCP_REGION")
+BQ_DATASET = os.environ.get("BQ_DATASET")
+BQ_REGION = os.environ.get("BQ_REGION")
